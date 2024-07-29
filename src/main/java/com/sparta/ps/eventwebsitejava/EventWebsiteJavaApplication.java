fe7debe2cb3c3f409c6194eea9955b7e0e413ee0
RayFFH <phoenix222smart@gmail.com>
2024-07-29 16:22:55 +0100

    initial commit

---
+++ src/main/java/com/sparta/ps/eventwebsitejava/EventWebsiteJavaApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.ps.eventwebsitejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventWebsiteJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventWebsiteJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/ps/eventwebsitejava/EventWebsiteJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.ps.eventwebsitejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventWebsiteJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
